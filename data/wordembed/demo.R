e99182e4cd9a71dc73fbc8e845228b2b29cb713a
“cjbarrie” <chrisjbarrie@googlemail.com>
2022-03-10 16:03:24 +0000

    Change weeks 8 9

---
+++ data/wordembed/demo.R
@@ -0,0 +1,20 @@
+library(janeaustenr)
+library(dplyr)
+library(quanteda)
+
+text <- janeaustenr::prideprejudice
+sentences <- text[10:11]
+sentences <- paste(sentences[1], sentences[2], sep = " ")
+text <- text[10:13030]
+
+toks_austen <- tokens(text)
+
+fcm(toks_austen, context = "window", window = 6, ordered = F)
+
+sentence <- paste(sentences, sentences)
+
+toks_austen <- tokens(sentence)
+
+fcm(toks_austen, context = "window", window = 6, ordered = F, tri = F) #note that 
+
+sentence
